c9895f27ed5e69b8fee72b172a10fd6db4567012
onceme <oncewosiwo@gmail.com>
2018-09-27 19:14:36 +0800

    Add:测试

---
+++ test/test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+/**
+  判断是否所有元素都是int
+ */
+func CheckIfInt(items... string) (bool,[]int,[]string) {
+	ifAllInt := true
+	var intItems[] int
+	var strItems[] string
+	for _, v := range items {
+		vint, err := strconv.Atoi(v)
+		fmt.Println(v)
+		fmt.Println(err)
+		if(err == nil){
+			intItems = append(intItems, vint)
+		}else{
+
+			ifAllInt = false
+			strItems = append(strItems, v)
+		}
+
+	}
+	return ifAllInt,intItems,strItems
+}
+
+func main(){
+	ifInt,intItems,strItems := CheckIfInt("1","2","a")
+
+	fmt.Println("ifInt: " + strconv.FormatBool(ifInt))
+	fmt.Println(intItems)
+	fmt.Println(strItems)
+
+}
